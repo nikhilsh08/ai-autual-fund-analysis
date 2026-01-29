f98faea0c2b8e1f048f6bd07fdad835fd3b03cc3
rajneesh <rajneeshkumar9674737@gmail.com>
2026-01-29 16:34:38 +0530

    update:middlewwere

--- src/server/middleweware.ts
+++ src/server/middleweware.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/server/auth/auth";
 import { NextResponse } from "next/server";
 
-const publicRoutes = ["/", "/auth/login", "/auth/register", "/auth/error"];
+const publicRoutes = ["/", "/sign-in", "/sign-up", "/auth/error"];
 const authRoutes = ["/auth/login", "/auth/register"];
 const apiAuthPrefix = "/api/auth";
 const DEFAULT_LOGIN_REDIRECT = "/dashboard";
@@ -28,6 +28,7 @@ export default auth((req) => {
   }
 
   // Redirect non-logged-in users to login for protected routes
+
   if (!isLoggedIn && !isPublicRoute) {
     let callbackUrl = nextUrl.pathname;
     if (nextUrl.search) {
@@ -36,7 +37,7 @@ export default auth((req) => {
 
     const encodedCallbackUrl = encodeURIComponent(callbackUrl);
     return NextResponse.redirect(
-      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+      new URL(`/sign-in?callbackUrl=${encodedCallbackUrl}`, nextUrl)
     );
   }
 
